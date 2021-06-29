db2c1ac66b9a08678bfdcf7891d4953668c12674
jiayen lee <jiiayenn.lee@gmail.com>
2021-06-29 22:41:48 +0800

    updated on about us and beautify the front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+root to: "pages#home"
+
+get "/menu", to: "pages#menu"
+get "/aboutus", to: "pages#about"
+
 end
